ff81a5c2729f40595eab0fa8857354099e717ee3
Emilien SANCHEZ <emilien.sanchez16@gmail.com>
2025-11-27 17:46:26 +0100

    ajout du laplacien par diffusion et fixe code

--- include/imGUIviewer.h
+++ include/imGUIviewer.h
@@ -4,7 +4,6 @@
 #include <vector>
 #include <string>
 
-// Forward declarations pour éviter les dépendances lourdes dans le .h
 namespace igl {
     namespace opengl {
         namespace glfw {
@@ -17,29 +16,30 @@ namespace igl {
 
 class ViewerControls {
 public:
-    // Méthode principale de rendu de l'interface
     void render(igl::opengl::glfw::imgui::ImGuiMenu& menu);
+    int selected_vertex = 0;
+    int k_rings = 0;
+    
+    bool boundary = false;
+    bool selection_mode = false;
 
 private:
-    // Énumération pour la direction
     enum Orientation { Up = 0, Down, Left, Right };
 
-    // Variables d'état de l'interface
-    double doubleVariable = 0.0;
+    // Variables
     bool boolVariable = true;
     Orientation direction = Up;
     int num_choices = 3;
     std::vector<std::string> choices;
     int idx_choice = 0;
 
-    // Méthodes de rendu des widgets individuels
-    void render_double_input();
+    void render_k_ring();
+    void render_boundary();
     void render_bool_checkbox();
     void render_direction_combo();
     void render_letter_selector();
     void render_hello_button();
 
-    // Méthode utilitaire
     void update_letter_choices();
 };
 
--- src/viewer.cpp
+++ src/viewer.cpp
@@ -7,8 +7,11 @@
 void ViewerControls::render(igl::opengl::glfw::imgui::ImGuiMenu& menu) {
     menu.draw_viewer_menu();
     
-    if (ImGui::CollapsingHeader("New Group", ImGuiTreeNodeFlags_DefaultOpen)) {
-        render_double_input();
+    if (ImGui::CollapsingHeader("Laplacien config", ImGuiTreeNodeFlags_DefaultOpen)) {
+        ImGui::Checkbox("Selection Mode", &selection_mode);
+
+        render_k_ring();
+        render_boundary();
         render_bool_checkbox();
         render_direction_combo();
         render_letter_selector();
@@ -16,44 +19,49 @@ void ViewerControls::render(igl::opengl::glfw::imgui::ImGuiMenu& menu) {
     }
 }
 
-void ViewerControls::render_double_input() {
-    ImGui::InputDouble("double", &doubleVariable, 0, 0, "%.4f");
+void ViewerControls::render_k_ring() {
+    ImGui::InputInt("k_rings", &k_rings);
+}
+
+
+void ViewerControls::render_boundary() {
+    ImGui::Checkbox("boundary", &boundary);
 }
 
 void ViewerControls::render_bool_checkbox() {
-    if (ImGui::Checkbox("bool", &boolVariable)) {
-        std::cout << "boolVariable: " << std::boolalpha << boolVariable << std::endl;
-    }
+    // if (ImGui::Checkbox("bool", &boolVariable)) {
+    //     std::cout << "boolVariable: " << std::boolalpha << boolVariable << std::endl;
+    // }
 }
 
 void ViewerControls::render_direction_combo() {
-    ImGui::Combo("Direction", (int*)(&direction), "Up\0Down\0Left\0Right\0\0");
+    // ImGui::Combo("Direction", (int*)(&direction), "Up\0Down\0Left\0Right\0\0");
 }
 
 void ViewerControls::render_letter_selector() {
-    if (ImGui::InputInt("Num letters", &num_choices)) {
-        num_choices = std::max(1, std::min(26, num_choices));
-    }
+    // if (ImGui::InputInt("Num letters", &num_choices)) {
+    //     num_choices = std::max(1, std::min(26, num_choices));
+    // }
     
-    if (num_choices != (int)choices.size()) {
-        update_letter_choices();
-    }
+    // if (num_choices != (int)choices.size()) {
+    //     update_letter_choices();
+    // }
     
-    ImGui::Combo("Letter", &idx_choice, choices);
+    // ImGui::Combo("Letter", &idx_choice, choices);
 }
 
 void ViewerControls::update_letter_choices() {
-    choices.resize(num_choices);
-    for (int i = 0; i < num_choices; ++i) {
-        choices[i] = std::string(1, 'A' + i);
-    }
-    if (idx_choice >= num_choices) {
-        idx_choice = num_choices - 1;
-    }
+    // choices.resize(num_choices);
+    // for (int i = 0; i < num_choices; ++i) {
+    //     choices[i] = std::string(1, 'A' + i);
+    // }
+    // if (idx_choice >= num_choices) {
+    //     idx_choice = num_choices - 1;
+    // }
 }
 
 void ViewerControls::render_hello_button() {
-    if (ImGui::Button("Print Hello", ImVec2(-1, 0))) {
-        std::cout << "Hello\n";
-    }
+    // if (ImGui::Button("Print Hello", ImVec2(-1, 0))) {
+    //     std::cout << "Hello\n";
+    // }
 }
